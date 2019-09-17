e998674ac24b1bca800c5f381348b97346e69fa4
Robel <rahail79@harrisonburg.k12.va.us>
2019-09-17 13:33:23 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('Robel', btnPress); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
@@ -11,4 +11,3 @@ function btnPress() {
   score++;
   postToPage(score);
 }
-
