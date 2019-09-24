403f98f7ca37e59fb081d781394a5081795c241a
Robel <rahail79@harrisonburg.k12.va.us>
2019-09-24 14:29:34 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var score = 1; // Create a variable to store the score in
 
 var title = new Title('Survival'); // Create a title element
 var button = new Button('hello', btnPress);
-var button = new Button('Wood', 🌲, btnPress);     // Create a button element
+var button = new Button('Knife🔪', btnPress);     // Create a button element
 var hidden = new Button('Boo!');
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
